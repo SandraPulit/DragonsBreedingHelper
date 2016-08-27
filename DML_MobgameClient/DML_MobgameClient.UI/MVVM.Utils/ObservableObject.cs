15ac13bd249d4545649670130826ccc2e35fdfd4
Sandra Pulit <sandrabeatapulit@gmail.com>
2016-08-27 19:35:03 +0200

    Added files

---
+++ DML_MobgameClient/DML_MobgameClient.UI/MVVM.Utils/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DML_MobgameClient.UI.Annotations;
+
+namespace DML_MobgameClient.UI.MVVM.Utils
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
